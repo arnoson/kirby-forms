71886ad2a9df779c7df0dc900bbae3f92c779927
arnoson <hallo@arno-schlipf.info>
2021-05-28 23:21:35 +0200

    Refactoring

--- lib/KirbyForms.php
+++ lib/KirbyForms.php
@@ -16,7 +16,10 @@ class KirbyForms {
    * @param Kirby\Cms\Page $formPage
    */
   function formRules($formPage): array {
-    $formRules = [];
+    $formRules = [
+      // `form_name` is a hidden field we send with every form.
+      'form_name' => [],
+    ];
     foreach ($formPage->form_fields()->toLayouts() as $layout) {
       foreach ($layout->columns() as $column) {
         foreach ($column->blocks() as $field) {
@@ -40,5 +43,65 @@ class KirbyForms {
       }
     }
     return $formRules;
+  }
+
+  /**
+   * Send a notification email containing the registration data.
+   * @param Uniform\Form $form
+   */
+  function sendNotificationEmail($form) {
+    if (option('arnoson.kirby-forms.notificationEmail.active')) {
+      $form->emailAction([
+        'to' => option('arnoson.kirby-forms.notificationEmail.to'),
+        'from' => option('arnoson.kirby-forms.notificationEmail.from'),
+        'subject' => option('arnoson.kirby-forms.notificationEmail.subject'),
+      ]);
+    }
+  }
+
+  /**
+   * Send a confirmation mail to the user. An email field has be present in the
+   * form data!
+   * @param Uniform\Form $form
+   */
+  function sendConfirmationEmail($form) {
+    if (
+      option('arnoson.kirby-forms.confirmationEmail.active') &&
+      $form->data('email')
+    ) {
+      $from = option('arnoson.kirby-forms.confirmationEmail.from');
+      $form->emailAction([
+        'to' => $form->data('email'),
+        'from' => $from,
+        'replyTo' => option(
+          'arnoson.kirby-forms.confirmationEmail.replyTo',
+          $from,
+        ),
+        'subject' => option('arnoson.kirby-forms.confirmationEmail.subject'),
+        'template' => 'form-registration-success',
+      ]);
+    }
+  }
+
+  function processRequest($formPage, $form) {
+    if (
+      kirby()
+        ->request()
+        ->is('POST')
+    ) {
+      $form->SaveJsonAction();
+      $this->sendNotificationEmail($form);
+      $this->sendConfirmationEmail($form);
+
+      if ($form->success()) {
+        // Even if no success page is set, we have to do a redirect to prevent
+        // another form submission on reload (Post/Redirect/Get pattern).
+        $successUrl =
+          fieldNotEmpty($formPage->form_success_page()->url()) ??
+          url($formPage->url(), ['params' => ['submit' => 'success']]);
+
+        go($successUrl);
+      }
+    }
   }
 }
