c6ebf7fff587ba371f0e271f362ec03734e52e78
arnoson <hallo@arno-schlipf.info>
2021-06-22 14:58:36 +0200

    Build

--- index.js
+++ index.js
@@ -10124,7 +10124,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44269" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43771" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
