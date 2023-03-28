4946fa90f84d51a2951cc5efb6100d4021aa62af
arnoson <hallo@arno-schlipf.info>
2023-03-28 16:48:58 +0200

    feat: provide email templates as select field

---
+++ blueprints/form-email-template.php
@@ -0,0 +1,18 @@
+<?php
+
+use Kirby\Filesystem\F;
+
+return function (\Kirby\Cms\App $kirby) {
+  $templates = array_map(
+    // Use `F::name()` two times, as email templates might have a double file
+    // extensions like `email.text.php`
+    fn($file) => F::name(F::name($file)),
+    glob($kirby->root('templates') . '/emails/*.php'),
+  );
+
+  return [
+    'label' => 'Template',
+    'type' => 'select',
+    'options' => $templates,
+  ];
+};
