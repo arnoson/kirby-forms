3c6234bb1e267f0be2e8df3791ea71b8ec591ee3
arnoson <hallo@arno-schlipf.info>
2023-05-09 18:37:28 +0200

    chore: format code

--- snippets/blocks/form-field-checkboxes.php
+++ snippets/blocks/form-field-checkboxes.php
@@ -1,25 +1,24 @@
 <?php $default = $block->default()->isEmpty()
   ? []
-  : array_map('trim', explode(',', $block->default()->value()));
-?>
+  : array_map('trim', explode(',', $block->default()->value())); ?>
 
 <fieldset <?= e($block->required()->toBool(), 'required') ?>>
   <legend><?= $label ?></legend>
-  <?php foreach($block->options()->toStructure() as $option): ?>
+  <?php foreach ($block->options()->toStructure() as $option): ?>
   <?php
-    $value = $option->value();
-    $optionId = "$id/$value";
-    $label = $option->text()->isEmpty() ? $value : $option->text();
-    $attributes = [
-      'name' => $block->name() . '[]',
-      'id' => $optionId,
-      'value' => $value,
-      'checked' => in_array($value, $default) ? true : null
-    ];
+  $value = $option->value();
+  $optionId = "$id/$value";
+  $label = $option->text()->isEmpty() ? $value : $option->text();
+  $attributes = [
+    'name' => $block->name() . '[]',
+    'id' => $optionId,
+    'value' => $value,
+    'checked' => in_array($value, $default) ? true : null,
+  ];
   ?>
   <div>
     <input type="checkbox" <?= attr($attributes) ?> />
     <label for="<?= $optionId ?>"><?= $label ?></label>
   </div>
-  <?php endforeach ?>
+  <?php endforeach; ?>
 </fieldset>
