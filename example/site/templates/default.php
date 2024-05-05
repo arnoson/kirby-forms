ccce03518f8cd8ee6ace0eb8363eca67719856b7
arnoson <hallo@arno-schlipf.info>
2024-05-05 18:45:55 +0200

    chore: update example

--- example/site/templates/default.php
+++ example/site/templates/default.php
@@ -1,39 +1,61 @@
-<style>
-.form-field-invalid {
-  border: 1px solid red;
-}
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= $site->title() ?></title>
+  <?= css('assets/pico.classless.min.css') ?>
+  <style>
+    :root {
+      --pico-font-size: 1rem;
+    }
 
-.form-row {
-  display: grid;
-  gap: 1em;
-  grid-template-columns: repeat(6, 1fr);
-}
+    html {
+      scrollbar-gutter: stable;
+    }
 
-.form-column {
-  grid-column: span var(--span);
-}
+    .form-row {
+      display: grid;
+      gap: var(--pico-spacing);
+      grid-template-columns: repeat(12, 1fr);
+    }
 
-.form-field {
-  display: flex;
-  flex-direction: column;
-}
+    .form-column {
+      grid-column: span var(--span);
+    }
 
-.form-field {
-  margin-bottom: 1em;
-}
+    .form-field {
+      display: flex;
+      flex-direction: column;
+    }
 
-.form-field-required label::after {
-  content: ' *';
-  color: red;
-}
+    .uniform__potty {
+      position: absolute;
+      left: -9999px;
+    }
+  </style>  
+</head>
+<body>
+  <main>
+    <h1><?= $page->title() ?></h1>
+    
+    <section>
+      <h2><?= page('forms/workshop')->title() ?></h2>
+      <?php snippet('form', ['formPage' => page('forms/workshop')]); ?>
+    </section>
 
-.uniform__potty {
-  position: absolute;
-  left: -9999px;
-}
-</style>
+    <hr>
+    
+    <section>
+      <h2><?= page('forms/contact')->title() ?></h2>
+      <p>To test multiple forms per page, we also add a little contact form which is independent of the workshop form above.</p>
+      <?php snippet('form', ['formPage' => page('forms/contact')]); ?>
+    </section>
+  </main>
+</body>
+</html>
 
-<h1><?= $page->title() ?></h1>
 
-<?php snippet('form', ['formPage' => page('forms/workshop')]); ?>
-<?php snippet('form', ['formPage' => page('forms/contact')]); ?>
+
+
+
