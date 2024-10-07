f4e3a8e3252aaa5a35f19e8072d9ad906419cd71
Cees van Egmond <cees.vanegmond@wearejust.com>
2024-10-07 21:03:17 +0200

    Feature: Enable Dutch Langauge

--- translations/index.php
+++ translations/index.php
@@ -1,4 +1,5 @@
 <?php return [
   'en' => Kirby\Data\Yaml::read(__DIR__ . '/en.yml'),
   'de' => Kirby\Data\Yaml::read(__DIR__ . '/de.yml'),
+  'nl' => Kirby\Data\Yaml::read(__DIR__ . '/nl.yml'),
 ];
