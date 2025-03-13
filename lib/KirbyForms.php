85796b217cffa24a34221d7607e3d0573bd4e17e
arnoson <hallo@arno-schlipf.info>
2025-03-13 11:35:45 +0100

    fix: re-enable `$form->done()`
    Removed it for debugging 🙈

--- lib/KirbyForms.php
+++ lib/KirbyForms.php
@@ -131,7 +131,7 @@ class KirbyForms {
       if ($successType === 'page' && $successUrl) {
         go($successUrl, 303);
       } else {
-        // $form->done();
+        $form->done();
       }
     }
   }
