b93e0481c6ea82aa887af43f86bdd1d23e2530ce
arnoson <hallo@arno-schlipf.info>
2025-04-09 10:09:30 +0200

    feat: correctly render non-field blocks in form

--- snippets/form-fields.php
+++ snippets/form-fields.php
@@ -9,10 +9,15 @@
     $name = $block->name()->value();
     $label = $block->label()->isNotEmpty() ? $block->label() : $name;
     $id = "$formSlug/field/$name";
-    $shortType = str_replace('form-field-', '', $block->type());
+    $type = $block->type();
+    $fieldPrefix = 'form-field-';
+    $shortType = str_replace($fieldPrefix, '', $type);
+    $isField = str_starts_with($type, $fieldPrefix);
     ?>
+
+    <?php if ($isField): ?>
     <div class="form-field" data-field-type="<?= $shortType ?>">
-      <?php snippet('blocks/' . $block->type(), [
+      <?php snippet("blocks/$type", [
         'form' => $form,
         'block' => $block,
         'id' => $id,
@@ -22,6 +27,10 @@
       <small class="form-field-error" id="<?= $id ?>/error"><?= $error ?></small>
       <?php endif; ?>
     </div>
+    <?php else: ?>
+    <?= $block ?>
+    <?php endif; ?>
+
     <?php endforeach; ?>
   </div>
   <?php endforeach; ?>
