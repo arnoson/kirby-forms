3515a078b5720c2321327554f2d8ce2fe21e83cd
André Pimpão <a.pimpao@programmator.dev>
2025-06-05 16:13:59 +0100

    feat: add pt_PT language

--- translations/index.php
+++ translations/index.php
@@ -2,4 +2,5 @@
   'en' => Kirby\Data\Yaml::read(__DIR__ . '/en.yml'),
   'de' => Kirby\Data\Yaml::read(__DIR__ . '/de.yml'),
   'nl' => Kirby\Data\Yaml::read(__DIR__ . '/nl.yml'),
+  'pt_PT' => Kirby\Data\Yaml::read(__DIR__ . '/pt_PT.yml'),
 ];
